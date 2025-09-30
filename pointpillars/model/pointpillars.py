d311360eab58bc96b7b069e98812fbca7d860500
mirakim-avikus <mira.kim@avikus.ai>
2025-09-30 09:50:30 +0900

    feat: adjust NMS threshold(0.01 -> 0.1) for better suppression and determinism

--- pointpillars/model/pointpillars.py
+++ pointpillars/model/pointpillars.py
@@ -272,7 +272,7 @@ class PointPillars(nn.Module):
 
         # val and test
         self.nms_pre = 100
-        self.nms_thr = 0.01
+        self.nms_thr = 0.1
         self.score_thr = 0.1
         self.max_num = 50
 
